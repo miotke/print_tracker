b83bbac68978d2ac7d020d4efb18c59d410e4aaa
Andrew Miotke <andrewmiotke@gmail.com>
2021-07-24 11:18:08 -0700

    Updated app

--- Print_Tracker/apps.py
+++ Print_Tracker/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class PrintTrackerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Print_Tracker'
+    name = 'print_tracker'
--- src/settings.py
+++ src/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'Print_Tracker'
+    'print_tracker'
 ]
 
 MIDDLEWARE = [
